55c78c8ad2473214b07bb75b7716b068e62b6a5d
Ossamadev0101 <137723487+Ossamadev0101@users.noreply.github.com>
2023-10-06 14:42:47 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# إعدادات لواجهة الواتساب
+WHATSAPP_URL = "https://web.whatsapp.com/"
+WAIT_TIME = 15  # زمن انتظار تحميل الصفحة
+
+# إعدادات API أو مفاتيح أخرى
+API_KEY = "your_api_key_here"
