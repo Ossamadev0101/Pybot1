278db04de39bec712d50d52091db12d6314084d4
Ossamadev0101 <137723487+Ossamadev0101@users.noreply.github.com>
2023-10-06 14:50:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from selenium import webdriver
 import time
 
 def run_whatsapp_bot():
-    # إعداد متصفح الويب
-    driver = webdriver.Chrome()  # يمكنك استخدام متصفح آخر
+    driver = webdriver.Chrome()
     driver.get("https://web.whatsapp.com/")
-
-    # انتظار حتى يتم تحميل صفحة واتساب
     time.sleep(15)
 
-    # القيام بإجراءات إضافية بعد تحميل الصفحة، مثل إرسال رسالة أو استجابة للرسائل
+    while True:
+        user_input = input("Enter your message (type 'exit' to quit): ")
 
-    # إغلاق متصفح الويب بعد الانتهاء
+        if user_input.lower() == 'exit':
+            break
+
+        # إذا كان المستخدم يرسل "hi"، قم بالرد بـ "hello"
+        if user_input.lower() == 'hi':
+            response = "hello"
+        else:
+            # إذا لم يتطابق أي شرط، يمكنك إضافة قواعد أخرى هنا
+            response = "I'm not sure how to respond to that."
+
+        # يمكنك استخدام response في الرد على واجهة الواتساب هنا
+        print("Bot's response:", response)
+
     driver.quit()
 
 if __name__ == "__main__":
